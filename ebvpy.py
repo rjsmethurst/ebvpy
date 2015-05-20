0c74f53421a4b21628480e54e88a478928f086d1
Becky Smethurst <becky@macbook-air-2.nat.physics.ox.ac.uk>
2015-05-20 18:28:57 +0100

    changes to function description

--- ebvpy.py
+++ ebvpy.py
@@ -49,7 +49,7 @@ def calc_xy_pos(l,b):
 		row pixel index position of the Schlegel et al. (1998) dust map. Numpy array with dimensions (N,) of the input array
 
 	"""
-	n = (b > 0).astype(int)
+	n = np.array(b > 0).astype(int)
 	n[n==0] = -1
 	x = 2048 * ((1-(n*np.sin(b)))**(0.5))*np.cos(l) + 2047.5
 	y = (-1)*2048 * n * ((1-(n*np.sin(b)))**(0.5))*np.sin(l) + 2047.5
@@ -57,16 +57,14 @@ def calc_xy_pos(l,b):
 
 
 def calc_ebv(ra, dec):
-		""" Given sky coordinates (ra, dec) this function calculates the corresponding E(B-V) value on that part of the sky using the
-		Schlegel et al. (1998) dust maps. 
+    """ Given sky coordinates (ra, dec) this function calculates the corresponding E(B-V) value on that part of the sky using the Schlegel et al. (1998) dust maps.
 
 	INPUTS
 	=-=-=-
 	:ra:
-		right ascension of sources, in units of degrees. Numpy array of dimensions (N,)
+		right ascension of sources, in units of degrees. Numpy array of dimensions (N,). Cannot take singular value, must be in list/array.
 	:dec:
-		declination of sources, in units of degrees. Numpy array of dimensions (N,)
-
+		declination of sources, in units of degrees. Numpy array of dimensions (N,). Cannot take singular value, must be in list/array.
 	OUTPUTS
 	=-=-=-=
 
@@ -75,29 +73,28 @@ def calc_ebv(ra, dec):
 	
 
 	"""
-	if os.path.exists('~/SFD_dust_4096_sgp.fits') == False:
-		wget.download('http://nebel.rc.fas.harvard.edu/mjuric/lsd-data/sfd-dust-maps/SFD_dust_4096_sgp.fits', out='~/SFD_dust_4096_sgp.fits')
-	else:
+    if os.path.exists('SFD_dust_4096_sgp.fits') == False:
+		wget.download('http://nebel.rc.fas.harvard.edu/mjuric/lsd-data/sfd-dust-maps/SFD_dust_4096_sgp.fits', out='SFD_dust_4096_sgp.fits')
+    else:
+        pass
+    if os.path.exists('SFD_dust_4096_ngp.fits') == False:
+		wget.download('http://nebel.rc.fas.harvard.edu/mjuric/lsd-data/sfd-dust-maps/SFD_dust_4096_ngp.fits', out='SFD_dust_4096_ngp.fits')
+    else:
 		pass
-	if os.path.exists('~/SFD_dust_4096_ngp.fits') == False:
-		wget.download('http://nebel.rc.fas.harvard.edu/mjuric/lsd-data/sfd-dust-maps/SFD_dust_4096_ngp.fits', out='~/SFD_dust_4096_ngp.fits')
-	else:
-		pass
-	ngp = fits.open('~/SFD_dust_4096_ngp.fits')
-	sgp = fits.open('~/SFD_dust_4096_sgp.fits')
-	c = SkyCoord(ra=ra, dec=dec)
-	x, y = calc_xy_pos(c.galactic.l.radian, c.galactic.b.radian)
-	ebv = np.zeros(len(ra))
-	for row in range(len(ebv)):
-		if c.galactic.b.radian[row] > 0:
-			ebv[row] = ngp[y[row], x[row]]
-		else:
-			ebv[row] = sgp[y[row], x[row]]
-	return ebv
+    ngp = fits.open('SFD_dust_4096_ngp.fits')[0].data
+    sgp = fits.open('SFD_dust_4096_sgp.fits')[0].data
+    c = SkyCoord(ra=ra, dec=dec,  unit='deg')
+    x, y = calc_xy_pos(c.galactic.l.radian, c.galactic.b.radian)
+    ebv = np.zeros(len(ra))
+    for row in range(len(ebv)):
+        if c.galactic.b.radian[row] > 0:
+            ebv[row] = ngp[y[row], x[row]]
+        else:
+            ebv[row] = sgp[y[row], x[row]]
+    return ebv
 
 def calc_color_correction(band, ebv):
-		""" Given the E(B-V) value and a photometric bandpass this function calculates the corresponding magnitude correction using pre-defined
-		or user defined A_b/E(B-V) values. 
+    """ Given the E(B-V) value and a photometric bandpass this function calculates the corresponding magnitude correction using pre-defined or user defined A_b/E(B-V) values.
 
 	INPUTS
 	=-=-=-
@@ -114,15 +111,15 @@ def calc_color_correction(band, ebv):
 	:ab
 		A_b magnitude correction for given bandpass, b. Numpy array with dimensions (N,) of the input array. 
 	"""
-	a_ebv = {"u": 4.239, "g":3.303, "r":2.285, "i":1.698, "z":1.263}
-	if type(band) == dict:
-		a_ebv.update(band)
-		ab = a_ebv[b.keys()[0]]*ebv
-	else:
-		try:
-			ab = e_ebv[band]*ebv
-		except KeyError:
-			print "Not a recognised band, try submitting your own band with value as a dictionary to the function e.g. {'nuv':8.18}. Currently available are "+str(a_ebv.keys())
-			sys.exit()
-	return ab
+    a_ebv = {"u": 4.239, "g":3.303, "r":2.285, "i":1.698, "z":1.263}
+    if type(band) == dict:
+        a_ebv.update(band)
+        ab = a_ebv[b.keys()[0]]*ebv
+    else:
+        try:
+            ab = a_ebv[band]*ebv
+        except KeyError:
+            print "Not a recognised band, try submitting your own band with value as a dictionary to the function e.g. {'nuv':8.18}. Currently available are "+str(a_ebv.keys())
+            sys.exit()
+    return ab
 
